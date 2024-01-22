f5b64cf87028b37514511b4c9584d1062bcce6f2
vyuba <preyealexander731@gmail.com>
2024-01-22 17:53:48 +0100

    fixed the tailwind library issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-// tailwind.config = {
-//     theme: {
-//       extend: {
-//         colors: {
-//           clifford: '#da373d',
-//         }
-//       }
-//     }
-//   }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mon': ['"Montserrat"', 'sans-serif',],
+        'bai': ['"Bai Jamjuree"', 'sans-serif',],
+      },
+      colors: {
+        mainColor: '#3F60D3',
+      }
+    },
+  },
+  plugins: [],
+}
