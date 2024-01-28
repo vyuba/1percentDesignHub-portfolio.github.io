6b952a0ecf870c47d6ec49ad8f5240fc2c797d22
vyuba <preyealexander731@gmail.com>
2024-01-28 16:49:49 +0100

    made dark mde functional

--- script.js
+++ script.js
@@ -6,25 +6,9 @@ const lightMode = document.querySelector('#lightMode');
 const themeContainer = document.querySelector('#themeContainer');
 
 
-themeContainer.addEventListener('click', ()=>{
-    if (darkMode.style.opacity === '1') {
-        darkMode.style.opacity = 0
-        lightMode.style.opacity = 1
-    } else {
-        darkMode.style.opacity = 1
-        lightMode.style.opacity = 0
-    }
-})
 
 
-// darkMode.addEventListener('click', ()=>[
-//     darkMode.style.opacity = 0 ,
-//     lightMode.style.opacity = 1
-// ])
-// lightMode.addEventListener('click', ()=>[
-//     lightMode.style.opacity = 0 ,
-//     darkMode.style.opacity = 1
-// ])
+
 
 slideOpen.addEventListener('click', () => {
     const slideMenu = document.querySelector('#slideMenu')
@@ -42,28 +26,66 @@ projectButt.addEventListener('click', closeSlideMenu);
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
-  } else {
+}else {
     document.documentElement.classList.remove('dark')
+}
+  
+  // // Whenever the user explicitly chooses light mode
+  // localStorage.theme = 'light'
+  
+  // // Whenever the user explicitly chooses dark mode
+  // localStorage.theme = 'dark'
+  
+  // // Whenever the user explicitly chooses to respect the OS preference
+  // localStorage.removeItem('theme')
+  
+  const themeSwitch = () =>{
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark")
+      localStorage.theme = 'light'
+    } else {
+      document.documentElement.classList.add("dark")
+      localStorage.theme = 'dark'
+    }
   }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
 
 
+
+// const toggleTheme = () => {
+//   const theme = localStorage.getItem("theme");
+//   console.log(theme);
+//   if(theme){
+//     if(theme === "dark"){
+//       localStorage.setItem(theme, "light")
+//     }else{
+//       localStorage.setItem(theme, "dark")
+//     }
+//   }else{
+//     console.log("no theme");
+//   }
+// }
+
+
+
+
+
+  themeContainer && themeContainer.addEventListener('click', ()=>{
+    if (darkMode.style.opacity === '1') {
+        darkMode.style.opacity = 0
+        lightMode.style.opacity = 1
+      } else {
+        darkMode.style.opacity = 1
+        lightMode.style.opacity = 0
+      }
+      themeSwitch()
+})
+  
   let loader = document.querySelector('#preloader');
   window.addEventListener('load', ()=>{
     setTimeout(() => {
         loader.style.display = 'none'; 
     }, 50);
   })
-
-
 let selectorArray = document.querySelectorAll('.selector')
 selectorArray.forEach(button =>{
     button.addEventListener('click', () => {
